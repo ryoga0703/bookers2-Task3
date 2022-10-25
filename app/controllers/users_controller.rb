49acd7ef8b5bf91055405ec9c7643bff846e07cc
ryoga <115019652+ryoga0703@users.noreply.github.com>
2022-10-25 08:23:30 +0000

    [add] bookのcreate機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new
   end
 
   def show
+    @book = Book.new
   end
 
   def edit
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,12 @@ class Book < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
 end
