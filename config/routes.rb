f4f47fefc0a6db32ddcc806ecbc66e18b622807c
ryoga <115019652+ryoga0703@users.noreply.github.com>
2022-10-27 02:39:43 +0000

    [Finish] 4-25

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'homes/about'
-  root to: "homes#top"
+  get 'home/about'
+  root to: "home#top"
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
 end
